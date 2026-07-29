2005e51de09c42dbde40e71b33f62f4c17183bd5
shivamcodelog <shivamcodelog@gmail.com>
2026-07-29 06:50:20 +0530

    made the input validator with Joi

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/user.controller.js"
+import validateUser from "../middlewares/inputValidator.js"
 
 const router = express.Router()
 
-router.post("/createuser",createUser)
+router.post("/createuser",validateUser ,createUser)
 router.get("/alluser",getAllUsers)
 router.get("/user/:id",getUserById)
-router.put("/updateuser/:id",updateUser)
+router.put("/updateuser/:id",validateUser,updateUser)
 router.delete("/deleteuser/:id",deleteUser)
 
 export default router;
